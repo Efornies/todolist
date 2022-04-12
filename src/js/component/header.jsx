e34f32169ad3ded2f880bccf248747cd2df96cd7
Efornies <eleforbel@gmail.com>
2022-04-12 08:14:49 +0000

    task

---
+++ src/js/component/header.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const Header = () => {
+	return (
+		<div className="text-center">
+			<label className="form-label mt-3">
+				<h3>To Do List</h3>
+			</label>
+		</div>
+	);
+};
+
+export default Header;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,18 @@
 import React from "react";
+import Header from "./header.jsx";
+import AddTask from "./Task.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div
+				className="block bg-light mx-auto rounded"
+				style={{ width: "450px" }}>
+				<div className="mb-3 w-50 pb-2 mx-auto mt-4">
+					<Header />
+					<AddTask />
+				</div>
+			</div>
 		</div>
 	);
 };
